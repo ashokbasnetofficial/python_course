a3b9100721ac0f77c4ee386a1cc4d620ae954e26
Ashok Basnet <87795084+ashokbasnetofficial@users.noreply.github.com>
2024-03-17 23:35:10 +0545

    day-1 is completed!

---
+++ Beginner_course/Day_1_Task.py
@@ -0,0 +1,13 @@
+print('Welcome to the Band Name Generator');
+city = input("What's name of the city you grew up in?");
+pet = input("What's your pet name?");
+print("Your band name could be"+" "+city+" "+pet);
+
+print("This repl has exited, run again");
+# Day-1 challenge 
+print("Day-1 string manipulation")
+print('string concation is done with the "+" sign') 
+print("hello"+"world")
+print("ashok basnet\n ashok basnet")
+
+print(len(input("find the length of your typed string")))
--- Beginner_course/hello.py
+++
@@ -1,2 +0,0 @@
-# print hello world in python 
-print("Hello World!")
