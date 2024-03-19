92ebaeccad7d3ebef1c9f3070ed283284c3ca0fd
Ashok Basnet <87795084+ashokbasnetofficial@users.noreply.github.com>
2024-03-19 17:44:53 +0545

    variable,type error, data types

--- Beginner_course/Day_2_Task.py
+++ Beginner_course/Day_2_Task.py
@@ -9,7 +9,7 @@ first_name ="Ashok";
 last_name="Basnet";
 # how should we get last alphabet 'k' from above name
 
-# print(first_name[4]) 
+print(first_name[4]) 
 
 # string concatination using + sign 
 # print(first_name+" "+last_name)
@@ -21,18 +21,18 @@ last_name="Basnet";
 
 #float
 
-# pi = 3.14
-# r = float(input("please input radius"));
-# A= pi*r*r
-# print("area of the circle is:",round(A,2));
+pi = 3.14
+r = float(input("please input radius"));
+A= pi*r*r
+print("area of the circle is:",round(A,2));
 
 
 #write a program to adds 2 digits of two digit number
-# num = 35
-# num = str(num);
-# first =int(num[0]);
-# second = int(num[1]);
-# print(first+second)
+num = 35
+num = str(num);
+first =int(num[0]);
+second = int(num[1]);
+print(first+second)
 
 
 # # math operation 
@@ -41,10 +41,10 @@ last_name="Basnet";
 # # 4/2 #divide
 # # 4**2 # power
 # # write a program that calculates the Body Mass Index(BMI) from a user's weight and height.
-# height = int(input("enter the height: "));
-# weight= int(input("enter the weight: "));
-# BIM = weight/ height**2
-# print("BIM=",BIM)
+height = int(input("enter the height: "));
+weight= int(input("enter the weight: "));
+BIM = weight/ height**2
+print("BIM=",BIM)
 
 # score =0;
 # score+=1;
@@ -62,3 +62,15 @@ total_months = remaining_years*12
 
 print(f"you have {total_days}, {total_week} weeks and {total_months} days left")
 
+#---------------- day-2 final task  -------------------
+# tips calcualtor 
+
+print("Welcome to the tip calcualtor.");
+total_bill = float(input("What was the total bill?"));
+total_person = int(input("How many people to split the bill?"));
+tips_percent = float(input("What percentage tip would you like to give? 10, 12, or 15?"));
+tips_percent = float(tips_percent/100);
+per_person_bill = float(total_bill/total_person);
+per_person_bill+=per_person_bill*tips_percent;
+per_person_bill=round(per_person_bill,2);
+print("each person will pay: ",per_person_bill);
